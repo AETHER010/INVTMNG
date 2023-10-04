60c70d3b38646ba19f1e7f89f63afd37fae2712a
HI MESH <heem.ace@gmail.com>
2023-10-04 11:47:08 +0545

    updates

--- src/utilities/api.jsx
+++ src/utilities/api.jsx
@@ -1,2 +1,3 @@
-export const Api_Url = `https://ims.itnepalsoultions.com.pujanrajrai.com.np`;
+// export const Api_Url = `https://ims.itnepalsoultions.com.pujanrajrai.com.np`;
 // export const Api_Url = `http://192.168.1.68:8000/`;
+export const Api_Url = `https://ims-pro.xyz`;
